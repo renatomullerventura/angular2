f1e5558b898eba19beeff3edd5ddc0eb993c4298
Renato Mulller Ventura <re.infotec@gmail.om>
2016-09-01 23:28:53 +0000

    3ª aula, usando interpolação

--- app/app.component.js
+++ app/app.component.js
@@ -11,12 +11,15 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
-        this.title = "Hello guys!";
+        this.title = {
+            name: "Hello guys!",
+            "description": "descrição qualquer"
+        };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>{{ title }}</h1>\n    <input type=\"text\" value=\"{{ title }}\" />"
+            template: "<h1>{{ title.name }} - {{ title.description }}</h1>\n    <input type=\"text\" [ngModel]=\"title.name\" />\n    <input type=\"text\" [(ngModel)]=\"title.description\" />"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,12 @@ import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>{{ title }}</h1>
-    <input type="text" value="{{ title }}" />`
+    template: `<h1>{{ title.name }} - {{ title.description }}</h1>
+    <input type="text" [ngModel]="title.name" />
+    <input type="text" [(ngModel)]="title.description" />`
 })
 export class AppComponent {
-    title = "Hello guys!";
+    title = {
+        name: "Hello guys!",
+        "description":"descrição qualquer"
 }
